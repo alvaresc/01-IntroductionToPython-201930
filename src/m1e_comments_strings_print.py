007d445cd14dc04d7271b2b3bae7f7322b805c6a
alvaresc <alvaresc@rose-hulman.edu>
2019-04-08 19:46:28 -0400

    Done

--- src/m1e_comments_strings_print.py
+++ src/m1e_comments_strings_print.py
@@ -26,24 +26,17 @@
 ###############################################################################
 import rosegraphics as rg
 
-# def find2(numbers):
-#     for k in range(len(numbers)):
-#         if numbers[k] < 0:
-#             return numbers[k]
-#     return None
-#
-# print(find2( [-3, 6, 1, 4, 9, 5] ))
 
-# win3 = rg.RoseWindow(300,300)
-# r = 8
-# s = 14
-# for k in range((s-r)//2+1):
-#     circle = rg.Circle(rg.Point(300,200),r)
-#     circle.attach_to(win3)
-#     r=r+2
-#     win3.render()
+
+
+# class Mini(object):
+#     def __init__(self,a):
+#         self.a = 3
+#         self.m = a + 5
 #
-# win3.close_on_mouse_click()
+#         print(self.m)
+#
+# m1 = Mini(10)
 
 # print('Hello, World')
 # print('hi there')
